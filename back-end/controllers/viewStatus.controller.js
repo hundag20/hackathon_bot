93613afd45377f7739282e833961407b3b587890
Hunda Guluma <HundaG@moenco.corp>
2022-10-21 02:15:47 +0300

    view status layout created

--- back-end/controllers/viewStatus.controller.js
+++ back-end/controllers/viewStatus.controller.js
@@ -1,18 +1,23 @@
 const Package = require("../models/Package.model");
 
-const editAd = async (ctx) => {
+const viewStatus = async (ctx) => {
   const data = await ctx.update.callback_query?.data;
   const packId = data.split("_")[2];
 
   const pack = await Package.query().findById(packId);
   const rep = {
     package: "type + purchase date",
-    total_posts_made: "x",
-    total_posts_rem: "y",
+    total_posts_made_per_group: "x",
+    total_posts_rem_per_group: "y",
     Ad_1: {
       title: "title",
+      posts_made_per_group: "x",
+      average_View_count: "y" /*
+      (tot view for all ad.id posts in group A) + (tot view for all ad.id posts in group B) / x
+      where x = number of ad.id posts in group A (num of ad.id posts z same in all groups)
+      */,
     },
   };
   const report = ``;
 };
-module.exports = editAd;
+module.exports = viewStatus;
