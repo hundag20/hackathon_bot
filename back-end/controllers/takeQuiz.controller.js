ccbb02f120359894691f0ac47c153e75a130da70
hundag20 <hundaguluma@gmail.com>
2023-03-11 11:11:33 +0300

    added intermediate states

--- back-end/controllers/takeQuiz.controller.js
+++ back-end/controllers/takeQuiz.controller.js
@@ -19,29 +19,29 @@ const pushAd = async (adObj) => {
 };
 
 const loadQuiz = async (userId) => {
-//check user exists and create if not
-const user = await User.query().where({telid: userId});
-if(!user || user.length < 1){
-    await User.query().insert({telId: userId})
-}
+  //check user exists and create if not
+  const user = await User.query().where({ telid: userId });
+  if (!user || user.length < 1) {
+    await User.query().insert({ telId: userId });
+  }
 
-//get quizId of any quiz he hasn't taken. if none, show message teling him to wait for new ones to be added
-//load quizes that user has taken
-const takenQuizes = await QuizUser.query().where({
-    user_id: userId
-});
-if(!takenQuizes || takenQuizes.length < 1){
+  //get quizId of any quiz he hasn't taken. if none, show message teling him to wait for new ones to be added
+  //load quizes that user has taken
+  const takenQuizes = await QuizUser.query().where({
+    user_id: userId,
+  });
+  if (!takenQuizes || takenQuizes.length < 1) {
     //has taken no quizes
     const allQuizes = await quiz.query();
     const firstQuiz = allQuizes[0];
-    return firstQuiz
-} else if(takenQuizes){
-}
-}
+    return firstQuiz;
+  } else if (takenQuizes) {
+  }
+};
 
 const loadQuestion = async (qId) => {
-    return await Question.query().findById(qId)
-}
+  return await Question.query().findById(qId);
+};
 
 //MARK-- WIZARD_newAd
 const takeQuiz_Wizard = new WizardScene(
@@ -49,41 +49,155 @@ const takeQuiz_Wizard = new WizardScene(
 
   //A. load quiz and show first question
   async (ctx) => {
-    try{
-        ctx.wizard.state.quizData = {};
-        ctx.wizard.state.quizData.userId = ctx.update?.message?.from?.id;
-        const quiz = await loadQuiz(ctx.wizard.state.quizData.userId);
-        const firstQ = await loadQuestion(quiz.q1_id);
-        const keys = Object.keys(firstQ);
-        const options = []
-        keys.forEach(key => {
-            if(key.includes('ans') && firstQ[key]){
-                options.push([`${firstQ[key]}`, key])
-            }
-        });
-        const letters = ['A', 'B', 'C', 'D', 'E']
-        let ansList = ''
-        options.forEach((el, i) => {
-            ansList = ansList.concat(`${letters[i]}. ${el[0]} \n`)
-        })
-        ctx.replyWithHTML(
-            `<span class='tg-spoiler'><b>Q1. ${firstQ.question} </b></span>\n\n${ansList}`            
-          ,
-            Extra.HTML().markup((m) =>
-              m.inlineKeyboard(options.map((el, i) => m.callbackButton(letters[i], letters[i])))
-            ),
-            {
-              parse_mode: "HTML",
-              reply_markup: {
-                forceReply: true,
-              },
-            }
-          );
-        return ctx.wizard.next();
-    }catch(err){
-
+    try {
+      ctx.wizard.state.quizData = {};
+      ctx.wizard.state.quizData.userId = ctx.update?.message?.from?.id;
+      const quiz = await loadQuiz(ctx.wizard.state.quizData.userId);
+      ctx.wizard.state.quizData.quizObj = quiz;
+      const firstQ = await loadQuestion(quiz.q1_id);
+      const keys = Object.keys(firstQ);
+      const options = [];
+      keys.forEach((key) => {
+        if (key.includes("ans") && firstQ[key]) {
+          options.push([`${firstQ[key]}`, key]);
+        }
+      });
+      const letters = ["A", "B", "C", "D", "E"];
+      let ansList = "";
+      options.forEach((el, i) => {
+        ansList = ansList.concat(`${letters[i]}. ${el[0]} \n`);
+      });
+      ctx.replyWithHTML(
+        `<span class='tg-spoiler'><b>Q1. ${firstQ.question} </b></span>\n\n${ansList}`,
+        Extra.HTML().markup((m) =>
+          m.inlineKeyboard(
+            options.map((el, i) => m.callbackButton(letters[i], letters[i]))
+          )
+        ),
+        {
+          parse_mode: "HTML",
+          reply_markup: {
+            forceReply: true,
+          },
+        }
+      );
+      return ctx.wizard.next();
+    } catch (err) {
+      console.log(err);
+    }
+    return ctx.wizard.next();
+  },
+  //B. load second Question
+  async (ctx) => {
+    try {
+      const ans = ctx.update?.callback_query?.data;
+      ctx.wizard.state.quizData.ans1 = ans;
+      const quest = await loadQuestion(ctx.wizard.state.quizData.quizObj.q2_id);
+      const keys = Object.keys(quest);
+      const options = [];
+      keys.forEach((key) => {
+        if (key.includes("ans") && quest[key]) {
+          options.push([`${quest[key]}`, key]);
+        }
+      });
+      const letters = ["A", "B", "C", "D", "E"];
+      let ansList = "";
+      options.forEach((el, i) => {
+        ansList = ansList.concat(`${letters[i]}. ${el[0]} \n`);
+      });
+      ctx.replyWithHTML(
+        `<span class='tg-spoiler'><b>Q2. ${quest.question} </b></span>\n\n${ansList}`,
+        Extra.HTML().markup((m) =>
+          m.inlineKeyboard(
+            options.map((el, i) => m.callbackButton(letters[i], letters[i]))
+          )
+        ),
+        {
+          parse_mode: "HTML",
+          reply_markup: {
+            forceReply: true,
+          },
+        }
+      );
+      return ctx.wizard.next();
+    } catch (err) {
+      console.log(err);
     }
   },
+  //C. load third Question
+  async (ctx) => {
+    try {
+      const ans = ctx.update?.callback_query?.data;
+      ctx.wizard.state.quizData.ans2 = ans;
+      const quest = await loadQuestion(ctx.wizard.state.quizData.quizObj.q3_id);
+      const keys = Object.keys(quest);
+      const options = [];
+      keys.forEach((key) => {
+        if (key.includes("ans") && quest[key]) {
+          options.push([`${quest[key]}`, key]);
+        }
+      });
+      const letters = ["A", "B", "C", "D", "E"];
+      let ansList = "";
+      options.forEach((el, i) => {
+        ansList = ansList.concat(`${letters[i]}. ${el[0]} \n`);
+      });
+      ctx.replyWithHTML(
+        `<span class='tg-spoiler'><b>Q3. ${quest.question} </b></span>\n\n${ansList}`,
+        Extra.HTML().markup((m) =>
+          m.inlineKeyboard(
+            options.map((el, i) => m.callbackButton(letters[i], letters[i]))
+          )
+        ),
+        {
+          parse_mode: "HTML",
+          reply_markup: {
+            forceReply: true,
+          },
+        }
+      );
+      return ctx.wizard.next();
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  //D. load fourth Question
+  async (ctx) => {
+    try {
+      const ans = ctx.update?.callback_query?.data;
+      ctx.wizard.state.quizData.ans3 = ans;
+      const quest = await loadQuestion(ctx.wizard.state.quizData.quizObj.q4_id);
+      const keys = Object.keys(quest);
+      const options = [];
+      keys.forEach((key) => {
+        if (key.includes("ans") && quest[key]) {
+          options.push([`${quest[key]}`, key]);
+        }
+      });
+      const letters = ["A", "B", "C", "D", "E"];
+      let ansList = "";
+      options.forEach((el, i) => {
+        ansList = ansList.concat(`${letters[i]}. ${el[0]} \n`);
+      });
+      ctx.replyWithHTML(
+        `<span class='tg-spoiler'><b>Q4. ${quest.question} </b></span>\n\n${ansList}`,
+        Extra.HTML().markup((m) =>
+          m.inlineKeyboard(
+            options.map((el, i) => m.callbackButton(letters[i], letters[i]))
+          )
+        ),
+        {
+          parse_mode: "HTML",
+          reply_markup: {
+            forceReply: true,
+          },
+        }
+      );
+      return ctx.wizard.next();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 );
 
 const takeQuiz = async (ctx) => {
